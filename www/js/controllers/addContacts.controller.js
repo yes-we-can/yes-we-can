e85f498c3dccc2e201f1e30912b147a2b1b90693
Allen Jacobson <allen@zinkerz.com>
2016-07-22 02:34:08 +0300

    basics

--- www/js/controllers/addContacts.controller.js
+++ www/js/controllers/addContacts.controller.js
@@ -1,34 +1,31 @@
 (function (angular) {
     'use strict';
 
-    angular.module('starter').controller('AddContactsController', ['$scope',
-        function ($scope) {
+    angular.module('starter').controller('AddContactsController', ['$scope', '$timeout',
+        function ($scope, $timeout) {
             $scope.contactsArr = [];
+            $scope.phoneNumberArr = [];
             $scope.addNewContact = function () {
-
                 if (navigator.contacts) {
                     navigator.contacts.pickContact(function (contact) {
-
                         $scope.phoneNumber = contact && contact.phoneNumbers[0] && contact.phoneNumbers[0].value ? contact.phoneNumbers[0].value : undefined;
-                        $scope.phoneNumber = $scope.phoneNumber.replace(/([() ])+/g, '');
-                        $scope.phoneNumber = $scope.phoneNumber.replace('+972','0');
+                        $scope.phoneNumber = $scope.phoneNumber.replace(/([() -])+/g, '');
+                        $scope.phoneNumber = $scope.phoneNumber.replace('+972', '0');
+                        $timeout(function () {
+                            $scope.phoneNumberArr.push($scope.phoneNumber);
+                        });
                         console.log('The following contact has been selected:' + JSON.stringify(contact));
                     }, function (err) {
                         console.log('Error: ' + err);
                     });
                 }
                 else {
-                    $scope.phoneNumber = '(052) 6620568';
-                }
-                if ($scope.phoneNumber) {
-                    $scope.phoneNumber = $scope.phoneNumber.replace(/([() ])+/g, '');
-                    $scope.phoneNumber = $scope.phoneNumber.replace('+972','0');
-                    $scope.phoneNumberArr = [];
+                    $scope.phoneNumber = '(052) 662-0568';
+                    $scope.phoneNumber = $scope.phoneNumber.replace(/([() -])+/g, '');
+                    $scope.phoneNumber = $scope.phoneNumber.replace('+972', '0');
                     $scope.phoneNumberArr.push($scope.phoneNumber);
-                    debugger;
-                    // display in ui
+                    console.log('The following contact has been selected:' + JSON.stringify(contact));
                 }
-
             }
         }
     ]);
