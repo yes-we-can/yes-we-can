cdd0c66616bfa8dc85451acaca6efb0a3c1c399e
assafhp <assafshp@zahav.net.il>
2016-07-21 16:32:27 +0300

    initial

---
+++ www/js/controllers/photoCtrl.js
@@ -0,0 +1,50 @@
+(function (angular) {
+    'use strict';
+
+    angular.module('starter.controllers').controller('PhotoCtrl', ['$scope', '$cordovaCamera', '$log',
+        function ($scope, $cordovaCamera, $log) {
+
+            $scope.takePhoto = function () {
+                var options = {
+                    quality: 75,
+                    destinationType: Camera.DestinationType.DATA_URL,
+                    sourceType: Camera.PictureSourceType.CAMERA,
+                    allowEdit: true,
+                    encodingType: Camera.EncodingType.JPEG,
+                    targetWidth: 300,
+                    targetHeight: 300,
+                    popoverOptions: CameraPopoverOptions,
+                    saveToPhotoAlbum: false
+                };
+
+                $cordovaCamera.getPicture(options).then(function (imageData) {
+                    $scope.imgURI = "data:image/jpeg;base64," + imageData;
+                }, function (err) {
+                    // An error occured. Show a message to the user
+                });
+            }
+
+            $scope.choosePhoto = function () {
+                var options = {
+                    quality: 75,
+                    destinationType: Camera.DestinationType.DATA_URL,
+                    sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+                    allowEdit: true,
+                    encodingType: Camera.EncodingType.JPEG,
+                    targetWidth: 300,
+                    targetHeight: 300,
+                    popoverOptions: CameraPopoverOptions,
+                    saveToPhotoAlbum: false
+                };
+
+                $cordovaCamera.getPicture(options).then(function (imageData) {
+                    $scope.imgURI = "data:image/jpeg;base64," + imageData;
+                }, function (err) {
+                    // An error occured. Show a message to the user
+                });
+            }
+
+
+        }
+    ]);
+})(angular);
