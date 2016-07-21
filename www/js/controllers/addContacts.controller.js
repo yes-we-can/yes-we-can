939ab52297fd1a6b91f516b26475aace2c802ecf
assafhp <assafshp@zahav.net.il>
2016-07-22 00:48:12 +0300

    fixes

--- www/js/controllers/addContacts.controller.js
+++ www/js/controllers/addContacts.controller.js
@@ -5,11 +5,24 @@
     function ($scope) {
     $scope.contactsArr = [];
       $scope.addNewContact = function () {
-        navigator.contacts.pickContact(function(contact){
-          console.log('The following contact has been selected:' + JSON.stringify(contact));
-        },function(err){
-          console.log('Error: ' + err);
-        });
+
+        if (navigator.contacts){
+          navigator.contacts.pickContact(function(contact){
+
+            var phoneNumber = contact && contact.phoneNumbers[0] && contact.phoneNumbers[0].value ? contact.phoneNumbers[0].value : undefined;
+            console.log('The following contact has been selected:' + JSON.stringify(contact));
+          },function(err){
+            console.log('Error: ' + err);
+          });
+        }
+        else { 
+          phoneNumber = '(052) 6620568';
+        }
+        if (phoneNumber){
+            // 'remove () and space'
+            // display in ui
+          }
+        
       }
     }
   ]);
