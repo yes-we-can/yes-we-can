b817ae40c415eaa8418909ecf0b5564dca34d59c
igormas <igor.masliansky@gmail.com>
2016-07-22 00:31:58 +0300

    phone number in storage

--- www/js/app.js
+++ www/js/app.js
@@ -63,5 +63,5 @@ angular.module('starter', [
         }
       })
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/signup');
+    $urlRouterProvider.otherwise('/app');
   });
