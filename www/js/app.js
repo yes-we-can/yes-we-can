ee6e98c5e2592a407e3a508897bc9c80097fa05a
igormas <igor.masliansky@gmail.com>
2016-07-21 22:33:52 +0300

    infra: - wiredep

--- www/js/app.js
+++ www/js/app.js
@@ -7,6 +7,7 @@
 angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers'])
 
 .run(function ($ionicPlatform) {
+  
     $ionicPlatform.ready(function () {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
