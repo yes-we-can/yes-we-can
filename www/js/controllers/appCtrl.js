3a5747c7d1960577eb7f58659e128262c435879f
igormas <igor.masliansky@gmail.com>
2016-07-22 00:14:17 +0300

    phone number in storage

--- www/js/controllers/appCtrl.js
+++ www/js/controllers/appCtrl.js
@@ -4,11 +4,10 @@
     angular.module('starter').controller('AppCtrl', [
         'UserSrv', '$state',
         function (UserSrv, $state) {
-            // var phoneNumber = '0508350501';
-
             var phoneNumber = localStorage.getItem('phoneNumber');
             if(phoneNumber === null){
                 alert('set phone number in local storage');
+                // localStorage.setItem('phoneNumber','0508350501')
             }
 
             UserSrv.getUserData(phoneNumber).then(function(userData){
