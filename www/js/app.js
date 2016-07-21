c258d63c2cd3a231722a08148950110d6326bb5e
Ran Shternin <ran.shternin@gmail.com>
2016-07-21 22:24:15 +0300

    - signup page basis

--- www/js/app.js
+++ www/js/app.js
@@ -6,75 +6,82 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers'])
 
-.run(function ($ionicPlatform) {
+  .run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        if (window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            cordova.plugins.Keyboard.disableScroll(true);
-
-        }
-        if (window.StatusBar) {
-            // org.apache.cordova.statusbar required
-            StatusBar.styleDefault();
-        }
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
     });
-})
+  })
 
-.config(function ($stateProvider, $urlRouterProvider) {
+  .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
 
-        .state('app', {
+      .state('app', {
         url: '/app',
         abstract: true,
         templateUrl: 'templates/menu.html',
         controller: 'AppCtrl'
-    })
-
-    .state('app.search', {
-            url: '/search',
-            views: {
-                'menuContent': {
-                    templateUrl: 'templates/search.html'
-                }
-            }
-        })
-        .state('app.photo', {
-            url: '/photo',
-            views: {
-                'menuContent': {
-                    templateUrl: 'templates/photo.html',
-                    controller: 'PhotoCtrl'
-                }
-            }
-        })
-        .state('app.browse', {
-            url: '/browse',
-            views: {
-                'menuContent': {
-                    templateUrl: 'templates/browse.html'
-                }
-            }
-        })
-        .state('app.playlists', {
-            url: '/playlists',
-            views: {
-                'menuContent': {
-                    templateUrl: 'templates/playlists.html',
-                    controller: 'PlaylistsCtrl'
-                }
-            }
-        })
-        .state('app.single', {
-            url: '/playlists/:playlistId',
-            views: {
-                'menuContent': {
-                    templateUrl: 'templates/playlist.html',
-                    controller: 'PlaylistCtrl'
-                }
-            }
-        });
+      })
+      .state('app.signup', {
+        url: '/signup',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/signup.html',
+            controller: 'SignupCtrl'
+          }
+        }
+      })
+      .state('app.search', {
+        url: '/search',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/search.html'
+          }
+        }
+      })
+      .state('app.photo', {
+        url: '/photo',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/photo.html',
+            controller: 'PhotoCtrl'
+          }
+        }
+      })
+      .state('app.browse', {
+        url: '/browse',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/browse.html'
+          }
+        }
+      })
+      .state('app.playlists', {
+        url: '/playlists',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlists.html',
+            controller: 'PlaylistsCtrl'
+          }
+        }
+      })
+      .state('app.single', {
+        url: '/playlists/:playlistId',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlist.html',
+            controller: 'PlaylistCtrl'
+          }
+        }
+      });
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/playlists');
-});
+    $urlRouterProvider.otherwise('/app/signup');
+  });
---
+++ www/js/controllers/signupCtrl.js
@@ -0,0 +1,13 @@
+(function (angular) {
+    'use strict';
+
+    angular.module('starter.controllers').controller('SignupCtrl', ['$scope',
+        function ($scope) {
+            console.log('dfsdfs!');
+
+          $scope.doSignup = function(){
+              debugger;
+          };
+        }
+    ]);
+})(angular);
